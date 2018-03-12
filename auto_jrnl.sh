fda93927d5e3bdf69c8dd0a7ec2796412c5fca98
Owen Stranathan <owen+github@appfigures.com>
2018-03-12 17:52:26 -0400

    what the what?

--- auto_jrnl.sh
+++ auto_jrnl.sh
@@ -4,9 +4,14 @@ jrnl $@
 
 cwd="$(pwd)"
 DATE=`date '+%Y-%m-%d %H:%M'`
-cd ~/projects/journal
-git add -u
-git commit -m "$DATE"
-git push
+{
+  cd ~/projects/journal
+  git add -u
+} & > /dev/null
+commit="$(sed -n '$p' <(git commit -m "$DATE"))"
+push="$(sed -n '$p' <(git push))"
+
+echo $commit
+echo $push
 cd ${cwd}
 
